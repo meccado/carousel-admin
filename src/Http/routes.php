c2b3610dc9a7381a488217858354cf4da2c69bbd
Meccado <tsw603gp@gmail.com>
2016-06-11 20:13:09 +0200

    fix route

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,8 +1,9 @@
 <?php
 //'prefix'=>'api/v1',
 Route::group([ 'prefix'			=>'admin',
-				'middleware' 	=> ['web', 'throttle'],
-				'namespace' 	=> 'App\Http\Controllers\Admin'], function(){
+				'middleware' 	=> ['web', 'throttle', 'auth', 'admin'],
+				'namespace' 	=> 'App\Http\Controllers\Admin'], 
+             function(){
       Route::resource('carousels', 'CarouselController', ['only' => ['index', 'show','create', 'store', 'edit', 'update', 'destroy']]);
   		Route::post('carousels/{carousels}/upload', ['as' => 'admin.carousels.upload', 'uses' => 'CarouselController@upload']);
   		Route::get('carousels/{carousels}/image-upload', ['as' => 'admin.carousels.image-upload', 'uses' => 'CarouselController@getUpload']);
